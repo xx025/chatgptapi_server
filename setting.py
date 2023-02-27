841f229a8ec9fee5cd0c38ceb03c1dafe34a7dc3
xx025 <71559822+xx025@users.noreply.github.com>
2023-02-27 16:36:41 +0800

    Update setting.py

--- setting.py
+++ setting.py
@@ -2,6 +2,8 @@
 
 host = 'localhost'  # 主机地址，如果在云端请填写云端ip地址
 port = '8010'  # 端口号
-# api路径
-user_path = 'user'
+
+user_path = 'user'  # api路径 表现形式为`ws://localhost:port/user`
 server_path = 'server'
+
+max_users = 3  # 最多支持用户连接数
