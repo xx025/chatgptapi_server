0f4ac7e2611f1a3d18f7bea91da28b908434981c
xx025 <71559822+xx025@users.noreply.github.com>
2023-02-27 21:29:02 +0800

    Update setting.py
    Correcting port number error.

--- setting.py
+++ setting.py
@@ -1,7 +1,7 @@
 # this is a setting file that you can set some base parameters
 
 host = 'localhost'  # 主机地址，如果在云端请填写云端ip地址
-port = '8010'  # 端口号
+port = 8010  # 端口号
 
 user_path = 'user'  # api路径 表现形式为`ws://localhost:port/user`
 server_path = 'server'
