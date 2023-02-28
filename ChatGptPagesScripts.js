e7e8c006152f85db345dbbe501dda32e6f6ce5b3
xx025 <71559822+xx025@users.noreply.github.com>
2023-02-28 10:04:51 +0800

    Update ChatGptPagesScripts.js

--- ChatGptPagesScripts.js
+++ ChatGptPagesScripts.js
@@ -22,10 +22,9 @@
     'use strict';
 
 
-    console.log(`// @name         ChatGPT-link-to-PythonWeb
-// @description  关注：https://github.com/xx025/browser-chatgptapi`)
+    console.log(`//ChatGPT-link-to-PythonWeb`)
 
-    let server_url = "ws://localhost:8010/server"
+    let server_url = "ws://localhost:8010/server?token=hkyH8Ldxf17l9N9FYBoa"
     // 如果只是本机测试，请勿做改变，当然注意端口号
     //  如果web服务器与脚本运行的客户端不在一个机器上，需要在chrome
     //  中关闭安全检查允许Https+ws这种允许方式
