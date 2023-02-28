3eef294bc1631c1ac365b93658d1dd4181d76713
xx025 <71559822+xx025@users.noreply.github.com>
2023-02-28 10:04:59 +0800

    Update server.py

--- message_forward/server.py
+++ message_forward/server.py
@@ -1,9 +1,8 @@
 import json
 from typing import Union
 
-from fastapi import APIRouter, Query, HTTPException, Depends
+from fastapi import APIRouter, Query, Depends
 from fastapi import WebSocket
-from starlette import status
 from starlette.websockets import WebSocketDisconnect
 
 from message_forward.manger import server_manger, r, user_manager
@@ -39,24 +38,26 @@ async def websocket_endpoint(ws: WebSocket, token: str = Depends(get_cookie_or_t
     server_manger[server_id] = ws
 
     for user_ws in user_manager.values():
-        await user_ws.send_json({'msg': 'The server is now available'})
+        await user_ws.send_json({'msg': f'The {server_id} is now available'})
     while True:
         try:
             queue_length = await r.llen('msgs')
             if queue_length > 0:
                 d1 = await r.rpop('msgs')
 
-                data = json.loads(d1.decode('utf-8'))
+                if d1:
+                    # 暂时不知道问什么会取出None
+                    data = json.loads(d1.decode('utf-8'))
 
-                user_id = data['user_id']
-                user_msg = json.loads(data['query_msg'])
-                print(user_id, user_msg)
-                await ws.send_json(user_msg)
-                ans1 = await ws.receive_json()
-                if user_manager.get(user_id):
-                    await user_manager[user_id].send_json(ans1)
-                else:
-                    print("User not found")
+                    user_id = data['user_id']
+                    user_msg = json.loads(data['query_msg'])
+                    print(user_id, user_msg)
+                    await ws.send_json(user_msg)
+                    ans1 = await ws.receive_json()
+                    if user_manager.get(user_id):
+                        await user_manager[user_id].send_json(ans1)
+                    else:
+                        print("User not found")
 
         except WebSocketDisconnect:
             print(f'{server_id} disconnected')
