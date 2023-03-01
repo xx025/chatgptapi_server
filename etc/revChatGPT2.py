048de4359e6da759571d47510ae50a83d8953d79
xx025 <71559822+xx025@users.noreply.github.com>
2023-03-01 13:22:38 +0800

    s

--- etc/revChatGPT2.py
+++ etc/revChatGPT2.py
@@ -11,9 +11,8 @@ class CG_Client(WebSocketClient):
 
     def received_message(self, resp: TextMessage):
         prompt = json.loads(str(resp)).get('msg', 'hello')
-        print(prompt)
 
-        chatbot = Chatbot(config={"access_token": accessToken })
+        chatbot = Chatbot(config={"access_token": accessToken})
         response = ""
         for data in chatbot.ask(prompt):
             response = data["message"]
@@ -24,7 +23,7 @@ class CG_Client(WebSocketClient):
 if __name__ == '__main__':
     ws = None
     try:
-        ws = CG_Client('ws://localhost:8010/server?token=r64XoPjdpVWPpSTrnin1')
+        ws = CG_Client('ws://127.0.0.1:8020/server?token=r64XoPjdpVWPpSTrnin1')
         ws.connect()
         ws.run_forever()
     except KeyboardInterrupt:
