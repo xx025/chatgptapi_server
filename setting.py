2ebc9bec711153ce39ff5cfda500afb8cb45762c
xx025 <71559822+xx025@users.noreply.github.com>
2023-03-01 12:01:17 +0800

    Update setting.py

--- setting.py
+++ setting.py
@@ -1,16 +1,43 @@
 # this is a setting file that you can set some base parameters
 
-host = 'localhost'  # 主机地址，如果在云端请填写云端ip地址
-port = 8010  # 端口号
+from configobj import ConfigObj
 
-user_path = 'user'  # api路径 表现形式为`ws://localhost:port/user`
-server_path = 'server'  # chatgpt对接的服务断开
+import os
 
-server_tokens = ['r64XoPjdpVWPpSTrnin1', 'hkyH8Ldxf17l9N9FYBoa']  # 为chat服务连接设置验证机制
+if os.path.exists('setting.cfg'):
+    setting_path = 'setting.cfg'
+else:
+    setting_path = 'example.setting.cfg'
 
-max_users = 3  # 最多支持用户连接数
+config = ConfigObj(setting_path, encoding='UTF-8')
+
+wbs = config.get('webserver', {})
+
+host = wbs.get('host') or 'localhost'
+# 主机地址，如果在云端请填写云端ip地址
+port = wbs.get('port') or 8010
+# 端口号
+port = int(port)
+
+user_path = wbs.get('user_path') or 'user'
+# api路径 表现形式为`ws://localhost:port/user`
+server_path = wbs.get('server_path') or 'server'
+# chatgpt对接的服务端口
+
+server_tokens = wbs.get('server_tokens') or ['r64XoPjdpVWPpSTrnin1', 'hkyH8Ldxf17l9N9FYBoa']
+# 为chat服务连接设置验证机制
+
+max_users = wbs.get('max_users') or 3  # 最多支持用户连接数
+max_users = int(max_users)
 
 # Redis 配置
-redis_host = '127.0.0.1'  # 主机地址
-redis_port = 6379  # 断开
-redis_db = 0  # 数据库序号
+
+reds = config.get('redis', {})
+redis_host = reds.get('host') or '127.0.0.1'
+# redis数据库地址
+redis_port = reds.get('port') or 6379  # 断开
+redis_port = int(redis_port)
+
+redis_db = reds.get('db') or 0
+# 数据库序号
+redis_db = int(redis_db)
